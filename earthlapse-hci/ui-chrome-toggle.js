c5f5b6a4999dc73a643dc4c0a8bef4d7a6903d60
Brian Liu Xu <blx@andrew.cmu.edu>
2016-03-20 17:59:02 -0400

    minor interface tweaks and hacky touch fix

--- earthlapse-hci/ui-chrome-toggle.js
+++ earthlapse-hci/ui-chrome-toggle.js
@@ -7,9 +7,13 @@
     var $machine = $("#timeMachine").append($toggleDiv);
     var $chrome = $(".vector-layers, .location_search_div").addClass("earthlapse-ui-chrome earthlapse-ui-chrome-optional");
 
+    var lastTouch = 0;
     $toggle.on("mousedown touchstart", function (e) {
         e.preventDefault();
+        var thisTouch = (new Date()).getTime();
+        if (thisTouch - lastTouch < 200) return;
         $machine.toggleClass("earthlapse-ui-minimal");
+        lastTouch = thisTouch;
     });
   });
 }(jQuery));
