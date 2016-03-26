517b7131f7c9b434fe36ac99ee4e9011c3c58cba
Brian Liu Xu <blx@andrew.cmu.edu>
2016-03-26 16:52:10 -0400

    also make zoom slider optional

--- earthlapse-hci/ui-chrome-toggle.js
+++ earthlapse-hci/ui-chrome-toggle.js
@@ -5,7 +5,7 @@
     var $toggle = $("<button type=\"button\" id=\"earthlapse-ui-chrome-toggle\" />");
     var $toggleDiv = $("<div class=\"earthlapse-ui-chrome earthlapse-ui-chrome-toggle-div\" />").append($toggle);
     var $machine = $("#timeMachine").append($toggleDiv);
-    var $chrome = $(".vector-layers, .location_search_div").addClass("earthlapse-ui-chrome earthlapse-ui-chrome-optional");
+    var $chrome = $(".vector-layers, .location_search_div, .sideToolBar").addClass("earthlapse-ui-chrome earthlapse-ui-chrome-optional");
 
     var lastTouch = 0;
     $toggle.on("mousedown touchstart", function (e) {
