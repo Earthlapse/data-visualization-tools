27d05e8981f19ec7d14667a982482237ad7a159a
Brian Liu Xu <blx@andrew.cmu.edu>
2016-04-06 12:15:10 -0400

    style changes for Earthlapse UI eventing

--- earthlapse-hci/earthlapse.js
+++ earthlapse-hci/earthlapse.js
@@ -1,39 +1,41 @@
 "use strict";
 
-var EarthlapseUI = (function ($) {
-  var events = {};
+var EarthlapseUI = (function($) {
+    var events = {};
 
-  function bind(evname, fn) {
-    if (!(evname in events)) {
-      events[evname] = [];
+    function bind(evname, fn) {
+        if (!(evname in events)) {
+            events[evname] = [];
+        }
+
+        // Don't bind handler if it was already bound
+        if (events[evname].indexOf(fn) > 0) {
+            return;
+        }
+
+        // Add to handlers list
+        events[evname].push(fn);
     }
 
-    if (events[evname].indexOf(fn) > 0) {
-      return;
+    function trigger(evname, options) {
+        if (!(evname in events)) {
+            return;
+        }
+
+        for (var i = 0; i < events[evname].length; i++) {
+            var _options = {};
+            for (var x in options) {
+                _options[x] = options[x];
+            }
+            events[evname][i](_options);
+        }
     }
 
-    events[evname].push(fn);
-  }
-
-  function trigger(evname, options) {
-    if (!(evname in events)) {
-      return;
-    }
-
-    for (var i = 0; i < events[evname].length; i++) {
-      var _options = {};
-      for (var x in options) {
-          _options[x] = options[x];
-      }
-      events[evname][i](_options);
-    }
-  }
-
-  return {
-    bind: bind,
-    trigger: trigger,
-    init: function (options) {
-      return trigger("init", options);
-    }
-  };
-}(jQuery));
+    return {
+        bind: bind,
+        trigger: trigger,
+        init: function(options) {
+            return trigger("init", options);
+        }
+    };
+} (jQuery));
