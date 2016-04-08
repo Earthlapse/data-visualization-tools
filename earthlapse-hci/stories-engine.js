fe57d0512fd1eb2ac143f0b91e1b92824de2b17c
Brian Liu Xu <blx@andrew.cmu.edu>
2016-04-08 13:30:49 -0400

    story mode progress

--- earthlapse-hci/stories-engine.js
+++ earthlapse-hci/stories-engine.js
@@ -6,183 +6,133 @@
     var storyId;   // string,
     var storyDict; // storyId -> keyframeArray
 
-    // Earth Engine Timelapse
-    var $landsatToggle = $("#landsat-base");
-    // Light Map
-    var $lightMapToggle = $("#light-base");
-    // Dark Map
-    var $darkMapToggle = $("#dark-base");
-    // Protected Areas
-    var $wdpaLayerToggle = $("#show-wdpa");
-    // Forest Loss By Year
-    var $globalForestLayerToggle = $("#show-forest-change");
-    // Forest Loss/Gain
-    var $globalForestLossLayerToggle = $("#show-forest-loss-gain");
-    // Animated Fotest Loss/Gain
-    var $animatedGlobalForestLossLayerToggle = $("#show-animated-forest-loss-gain");
-    // Forest Loss Alerts
-    var $forestLossAlarmsLayerToggle = $("show-forest-alerts");
-    // Forest Loss Alerts No Overlay
-    var $forestLossAlarmsNoOverlayLayerToggle = $("show-forest-alerts-no-overlay");
-    // Fires at Night
-    var $viirsLayerToggle = $("#show-viirs");
-    // Lights at Night
-    var $lightsAtNightToggle = $("#show-lights-at-night");
-    // Coral Bleaching
-    var $coralLayerToggle = $("#show-coral");
-    // Coral Bleaching Alerts
-    var $coralBleachingAlertsLayerToggle = $("#show-coral-bleaching-alerts");
-    // Wind Power
-    var $usgsWindTurbineLayerToggle = $("#show-usgs-windturbine");
-    // Solar Power
-    var $solarInstallsLayerToggle = $("#show-solar-installs");
-    // Drilling
-    var $drillingLayerToggle = $("#show-drilling");
-    // Himawari-8
-    var $himawariLayerToggle = $("#show-himawari");
-    // Water Occurrence
-    var $waterOccurrenceToggle = $("#show-water-occurrence");
-    // Water Change
-    var $waterChangeToggle = $("#show-water-change");
-
+    var defaultMap = "landsat-base";
+    var maps, layers;
 
     /* Functions */
-    function setLayers(layers) {
+    function setLayers(requestedLayers) {
         clearLayers();
-        for(var i=0;i<layers.length;i++){
-            enableLayer(layers[i]);
+        for (var i = 0; i < requestedLayers.length; i++) {
+            enableLayer(requestedLayers[i]);
         }
     }
+
     function enableLayer(layerName) {
-        $(layerName).click();
+        layers[layerName].$dom.click();
     }
 
     function clearLayers() {
-        //copied from index.html
-
         // Default to Landsat turned on
-        $landsatToggle.click();
+        maps[defaultMap].$dom.click();
 
-        if ($wdpaLayerToggle.prop('checked')) {
-          $wdpaLayerToggle.click();
-        }
-        if ($globalForestLayerToggle.prop('checked')) {
-          $globalForestLayerToggle.click();
-        }
-        if ($globalForestLossLayerToggle.prop('checked')) {
-          $globalForestLossLayerToggle.click();
-        }
-        if ($animatedGlobalForestLossLayerToggle.prop('checked')) {
-          $animatedGlobalForestLossLayerToggle.click();
-        }
-        if ($forestLossAlarmsLayerToggle.prop('checked')) {
-          $forestLossAlarmsLayerToggle.click();
-        }
-        if ($forestLossAlarmsNoOverlayLayerToggle.prop('checked')) {
-          $forestLossAlarmsNoOverlayLayerToggle.click();
-        }
-        if ($lightsAtNightToggle.prop('checked')) {
-          $lightsAtNightToggle.click();
-        }
-        if ($coralLayerToggle.prop('checked')) {
-          $coralLayerToggle.click();
-        }
-        if ($coralBleachingAlertsLayerToggle.prop('checked')) {
-          $coralBleachingAlertsLayerToggle.click();
-        }
-        if ($usgsWindTurbineLayerToggle.prop('checked')) {
-          $usgsWindTurbineLayerToggle.click();
-        }
-        if ($solarInstallsLayerToggle.prop('checked')) {
-          $solarInstallsLayerToggle.click();
-        }
-        if ($drillingLayerToggle.prop('checked')) {
-          $drillingLayerToggle.click();
-        }
-        if ($waterOccurrenceToggle.prop('checked')) {
-          $waterOccurrenceToggle.click();
-        }
-        if ($waterChangeToggle.prop('checked')) {
-          $waterChangeToggle.click();
+        for (var key in layers) {
+            var $layerToggle = layers[key].$dom;
+            if ($layerToggle.prop("checked")) {
+                $layerToggle.click();
+            }
         }
     }
+
     function setMap(mapName) {
-        $(mapName).click();
+        maps[mapName].$dom.click();
     }
-    function clearMap(mapName) {
-        if (!$landsatToggle.prop('checked')) {
-          $landsatToggle.click();
-        }        
+
+    function clearMap() {
+        maps[defaultMap].$dom.click();
     }
+
     function setText() {
         $('').text(storyDict[storyId][index]);
     }
+
     function showTextControlButton() {
         $('').show();
     }
+
     function hideTextControlButton() {
         $('').hide();
-
     }
+
     function isInView() {
 
     }
 
     function startStory(storyNumber) {
-        buildTimeline();
-        storyId=storyNumber;
-        index=0;
-        goToKeyframe(index);
-        setMap(storyDict[storyId][0]["Map"]);;
-        setLayers(storyDict[storyId][0]["Layers"]);
-        showTextControlButton();
-        // anything else that needs to be done
-    }
+        storyId = storyNumber;
 
-    function endStory() {
+        // Reset story mode
+        buildTimeline();
         clearMap();
         clearLayers();
         hideTextControlButton();
+
+        // Rewind story
+        goToKeyframe(0);
     }
 
-    function goToKeyframe(Frameindex) {
-        //Hide the back or next button depends on the index availability
-        index=Frameindex;
-        setNewView(storyDict[storyId][index]['Bounding_box']);
-        if(storyDict[storyId][index]['startPause']){
-            pause();
+    function endStory() {
+        Earthlapse.Modes.changeModeTo("menu");
+    }
+
+    function goToKeyframe(newIndex) {
+        // Check keyframe index bounds
+        if (newIndex < 0 || newIndex > storyDict[storyId].length - 1) {
+            throw "[Earthlapse.Stories] Keyframe index " + newIndex + " is out of bounds";
         }
-        else{
+
+        index = newIndex;
+
+        // Hide the back or next button depends on the index availability
+        if (index + 1 > storyDict[storyId].length - 1) {
+            $('#nextButton').hide();
+        }
+        if (index - 1 < 0) {
+            $('#backButton').hide();
+        }
+
+        setNewView(storyDict[storyId][index]['BoundingBox']);
+        setMap(storyDict[storyId][0]["Map"]);
+        setLayers(storyDict[storyId][0]["Layers"]);
+        showTextControlButton();
+
+        // Should we seek to another frame?
+        if (storyDict[storyId][index]['StopFrame'] >= 0) {
+            pause();
+            seek(storyDict[storyId][index]['StopFrame']);
+        }
+
+        // Should we pause the timelapse?
+        if (!storyDict[storyId][index]["Pause"]) {
             play();
         }
     }
+
     function nextKeyframe() {
-        if(index+1===storyDict[storyId].length){
-            $('#nextButton').hide();
-        }
-        if(index+1<=storyDict[storyId].length){
-            goToKeyframe(index+1);
-        }
+        goToKeyframe(index + 1);
     }
+
     function prevKeyframe() {
-        if(index-1===0){
-            $('#backButton').hide();
-        }
-        if(index-1>=0){
-            goToKeyframe(index-1);
-        }
+        goToKeyframe(index - 1);
     }
+
     function setPlaybackRate(rate) {
         timelapse.setPlaybackRate(rate);
     } // send to timelapse
+
     function setNewView(boundingBox) {
         timelapse.setNewView(boundingBox);
     } // send to timelapse
+
     function play() {
         timelapse.play();
     } // send to timelapse
+
     function pause() {
         timelapse.pause();
+    } // send to timelapse
+
+    function seek(timeInSec) {
+        timelapse.seek(timeInSec);
     } // send to timelapse
 
     function buildTimeline() {
@@ -191,11 +141,63 @@
         // attach to story mode container
     }
 
-    /* Bindings */
+    function initTimeline() {
+        timelapse.addTimeChangeListener(function (e) {
+            console.log(e);
+        });
+    }
+
+    function loadLayers() {
+        maps = {
+            // Earth Engine Timelapse
+            "landsat-base": { $dom: $("#landsat-base") },
+            // Light Map
+            "light-base": { $dom: $("#light-base") },
+            // Dark Map
+            "dark-base": { $dom: $("#dark-base") }
+        };
+
+        layers = {
+            // Protected areas
+            "show-wdpa": { $dom: $("#show-wdpa") },
+            // Forest Loss By Year
+            "show-forest-change": { $dom: $("#show-forest-change") },
+            // Forest Loss/Gain
+            "show-forest-loss-gain": { $dom: $("#show-forest-loss-gain") },
+            // Animated Fotest Loss/Gain
+            "show-animated-forest-loss-gain": { $dom: $("#show-animated-forest-loss-gain") },
+            // Forest Loss Alerts
+            "show-forest-alerts": { $dom: $("#show-forest-alerts") },
+            // Forest Loss Alerts No Overlay
+            "show-forest-alerts-no-overlay": { $dom: $("#show-forest-alerts-no-overlay") },
+            // Fires at Night
+            "show-viirs": { $dom: $("#show-viirs") },
+            // Lights at Night
+            "show-lights-at-night": { $dom: $("#show-lights-at-night") },
+            // Coral Bleaching
+            "show-coral": { $dom: $("#show-coral") },
+            // Coral Bleaching Alerts
+            "show-coral-bleaching-alerts": { $dom: $("#show-coral-bleaching-alerts") },
+            // Wind Power
+            "show-usgs-windturbine": { $dom: $("#show-usgs-windturbine") },
+            // Solar Power
+            "show-solar-installs": { $dom: $("#show-solar-installs") },
+            // Drilling
+            "show-drilling": { $dom: $("#show-drilling") },
+            // Himawari-8
+            "show-himawari": { $dom: $("#show-himawari") },
+            // Water Occurrence
+            "show-water-occurrence": { $dom: $("#show-water-occurrence") },
+            // Water Change
+            "show-water-change": { $dom: $("#show-water-change") }
+        };
+    }
+
     EarthlapseUI.bind("init", function() {
+        loadLayers();
+
         // Ajax download stories here
     });
-    // todo: bind to video time events
     // todo: bind to pan events
 
     // Expose Story Mode API
