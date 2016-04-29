294640b7e75ba02877eafa92592a073081bc375d
Brian Liu Xu <blx@andrew.cmu.edu>
2016-04-28 23:52:36 -0400

    add network connection check

--- earthlapse-hci/startup.js
+++ earthlapse-hci/startup.js
@@ -1,6 +1,9 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function(event) {
+    var EARTH_TIMELAPSE_CONFIG = EARTH_TIMELAPSE_CONFIG || {};
+    var startupConnectionThreshold = (typeof(EARTH_TIMELAPSE_CONFIG.startupConnectionThreshold) !== "number") ? 0 : EARTH_TIMELAPSE_CONFIG.startupConnectionThreshold;
+
     var oSplash = document.createElement("DIV");
     oSplash.id = "earthlapse-ui-splash";
 
@@ -26,11 +29,59 @@ document.addEventListener("DOMContentLoaded", function(event) {
     oScreen.appendChild(oSplash);
     document.body.appendChild(oScreen);
 
-    window.onload = function () {
-        if (typeof EarthlapseUI === "undefined") {
-            window.location.replace("../examples/webgl-timemachine/index.html");
-        } else {
-            oScreen.className = "earthlapse-ui-splash-complete";
+    // Status messages
+    if (typeof EarthlapseUI === "undefined") {
+        oStatus.innerHTML = "Checking network connection...";
+
+        var threshold = startupConnectionThreshold;
+        var succeeded = -1;
+
+        var oTestImage = document.createElement("IMG");
+
+        function enqueueNetworkTest() {
+            var setImageSrc = function () {
+                oTestImage.src = "http://thumbnails.cmucreatelab.org/thumbnail?root=http://earthengine.google.org/timelapse/data/20130507&boundsLTRB=239833.73508091227,522897.15176901396,243314.48721083216,524958.4096709508&width=264&height=204&frameTime=2.1&now=" + Math.random();
+            };
+
+            if (succeeded < 0) {
+                succeeded = 0;
+                setImageSrc();
+            } else {
+                setTimeout(setImageSrc, 500);
+            }
         }
-    };
+
+        function waitForNetwork() {
+            if (threshold <= 0 || succeeded >= threshold) {
+                oStatus.innerHTML = "Starting up...";
+                window.location.replace("../examples/webgl-timemachine/index.html");
+                return;
+            }
+
+            if (succeeded > 0) {
+                var progress = Math.round(100 * succeeded / threshold);
+                oStatus.innerHTML = "Checking network connectivity... " + progress + "%";
+            } else {
+                oStatus.innerHTML = "Waiting for network connection...";
+            }
+            enqueueNetworkTest();
+        }
+
+        oTestImage.onabort = oTestImage.onerror = function () {
+            succeeded = 0;
+            waitForNetwork();
+        };
+        oTestImage.onload = function () {
+            succeeded++;
+            waitForNetwork();
+        };
+
+        waitForNetwork();
+    } else {
+        oStatus.innerHTML = "Preparing user interface...";
+        window.onload = function () {
+            oScreen.className = "earthlapse-ui-splash-complete";
+            return;
+        };
+    }
 });
